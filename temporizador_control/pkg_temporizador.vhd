9bf2001a4ec31ef839a1aa00b156c276eb538ead
Maykol Felipe Muñoz <90796259+Dota117@users.noreply.github.com>
2026-09-21 01:26:28 -0500

    Update pkg_temporizador.vhd

--- temporizador_control/pkg_temporizador.vhd
+++ temporizador_control/pkg_temporizador.vhd
@@ -3,3 +3,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 package pkg_temporizador is
+  
+    constant CICLOS : integer := 50_000_000;
+
+    component divisor_reloj is
+        Port ( 
+            clk_in  : in  STD_LOGIC;
+            rst     : in  STD_LOGIC;
+            clk_1hz : out STD_LOGIC
+        );
+    end component;

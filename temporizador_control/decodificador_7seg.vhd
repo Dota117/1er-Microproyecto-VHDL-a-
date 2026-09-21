1a607cd9ef0d97432a7a7c76a611dd1f59044345
Maykol Felipe Muñoz <90796259+Dota117@users.noreply.github.com>
2026-09-21 01:04:20 -0500

    Update decodificador_7seg.vhd

--- temporizador_control/decodificador_7seg.vhd
+++ temporizador_control/decodificador_7seg.vhd
@@ -7,3 +7,20 @@ entity decodificador_7seg is
         seg_out: out STD_LOGIC_VECTOR(6 downto 0)
     );
 end decodificador_7seg;
+
+architecture Dataflow of decodificador_7seg is
+begin
+    with bcd_in select
+        seg_out <=  "0111111" when "0000", 
+                    "0000110" when "0001", 
+                    "1011011" when "0010", 
+                    "1001111" when "0011", 
+                    "1100110" when "0100", 
+                    "1101101" when "0101", 
+                    "1111101" when "0110", 
+                    "0000111" when "0111", 
+                    "1111111" when "1000", 
+                    "1101111" when "1001", 
+                    "0000000" when others; 
+end Dataflow;
+    

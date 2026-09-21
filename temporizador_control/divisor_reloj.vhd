2135744660eef78e99700d6037b9e1cb97cb0faf
Maykol Felipe Muñoz <90796259+Dota117@users.noreply.github.com>
2026-09-21 08:30:39 -0500

    Update divisor_reloj.vhd

--- temporizador_control/divisor_reloj.vhd
+++ temporizador_control/divisor_reloj.vhd
@@ -11,13 +11,13 @@ entity divisor_reloj is
 end divisor_reloj;
 
 architecture arch1 of divisor_reloj is
-    constant mitad_ciclos : integer := 50000000 / 2;
+    constant mitad_ciclos : integer := 25000000;
     signal conteo : integer range 0 to Mitad_ciclos := 0;
     signal estado_clk : STD_LOGIC := '0';
 begin
     process (clk_in,  rst)
     if rst '1' then
-        conteo <= 0
+        conteo <= 0;
         estado_clk <=  0;
     elsif rising.edge(clk_in) then
         if conteo = (mitad_ciclos -1) then

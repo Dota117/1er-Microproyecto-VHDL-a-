44c6a65632a20ece3c9c090ca2a0c2c4c8229413
Maykol Felipe Muñoz <90796259+Dota117@users.noreply.github.com>
2026-09-21 01:43:59 -0500

    Update divisor_reloj.vhd

--- temporizador_control/divisor_reloj.vhd
+++ temporizador_control/divisor_reloj.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.pkg_temporizador.all; 
 
 entity divisor_reloj is
     Port ( 
@@ -7,3 +8,12 @@ entity divisor_reloj is
         rst     : in  STD_LOGIC;
         clk_1hz : out STD_LOGIC
     );
+end divisor_reloj;
+
+architecture arch1 of divisor_reloj is
+    constant mitad_ciclos : integer := ciclos_1Hz / 2;
+    signal conteo : integer range 0 to Mitad_ciclos := 0;
+    signal estado_clk : STD_LOGIC := '0';
+
+
+                

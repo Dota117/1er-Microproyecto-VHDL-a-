9a6374efc6755488e94a295ba646e4d27756d344
Maykol Felipe Muñoz <90796259+Dota117@users.noreply.github.com>
2026-09-23 09:34:31 -0500

    Update contador_segundos.vhd

--- temporizador_control/contador_segundos.vhd
+++ temporizador_control/contador_segundos.vhd
@@ -3,7 +3,7 @@ use IEEE.STD.LOGIC_1164.ALL;
 
 entity contador_segundos is
     Port (
-        clk     : in  STD_LOGIC;
+        clk_1hz : in  STD_LOGIC;
         rst     : in  STD_LOGIC;
         ena     : in  STD_LOGIC;
         tiempo  : out integer range 0 to 255
@@ -13,11 +13,11 @@ end contador_segundos;
 architecture arch2 of contador_segundos is
     signal conteo_interno : integer range 0 to 255 := 0;
 begin
-    process(clk, rst)
+    process(clk_1hz, rst)
     begin
         if rst = '1' then
             conteo_interno <= 0;
-        elsif rising_edge(clk) then
+        elsif rising_edge(clk_1hz) then
             if ena = '1' then
                 if conteo_interno < 255 then
                     conteo_interno <= conteo_interno + 1;

50a224a40dc8b4b024360fba2402efe0691d92b6
Maykol Felipe Muñoz <90796259+Dota117@users.noreply.github.com>
2026-09-23 10:31:39 -0500

    Update control_gen.vhd

--- temporizador_control/control_gen.vhd
+++ temporizador_control/control_gen.vhd
@@ -1,7 +1,17 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL; 
+use IEEE.STD_LOGIC_1164.ALL;
 entity control_gen is
     Port (
         clk          : in  STD_LOGIC; 
         rst          : in  STD_LOGIC; 
         sensor_pres  : in  STD_LOGIC; 
+        tiempo_base  : in  integer range 0 to 255; 
+        
+        ena_timer_b  : out STD_LOGIC; 
+        ena_timer_ex : out STD_LOGIC; 
+        rst_timers   : out STD_LOGIC; 
+        led_alarma   : out STD_LOGIC; 
+        led_felicit  : out STD_LOGIC
+    );
+end control_gen;
+architecture arch_3 of control_gen is 

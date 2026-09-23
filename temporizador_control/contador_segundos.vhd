f9316d902b81837fe931ef1b55dfe94bd91349e7
Maykol Felipe Muñoz <90796259+Dota117@users.noreply.github.com>
2026-09-23 07:51:19 -0500

    Update contador_segundos.vhd

--- temporizador_control/contador_segundos.vhd
+++ temporizador_control/contador_segundos.vhd
@@ -13,7 +13,16 @@ end contador_segundos;
 architecture arch2 of contador_segundos is
     signal conteo_interno : integer range 0 to 255 := 0;
 begin
-    process(clk , rst)
+    process(clk, rst)
     begin
         if rst = '1' then
             conteo_interno <= 0;
+        elsif rising_edge(clk) then
+            if ena = '1' then
+                if conteo_interno < 255 then
+                    conteo_interno <= conteo_interno + 1;
+                end if;
+            end if;
+        end if;
+    end process;
+end end arch2;
